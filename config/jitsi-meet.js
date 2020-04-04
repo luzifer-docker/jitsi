b1a966b452a63426da63e3c6ae5f109d7eff8985
Knut Ahlers <knut@ahlers.me>
2020-04-04 17:51:05 +0200

    Import more of default jitsi config.js
    Signed-off-by: Knut Ahlers <knut@ahlers.me>

--- config/jitsi-meet.js
+++ config/jitsi-meet.js
@@ -1,9 +1,30 @@
 var config = {
+
+  // Connection
+  bosh: '//{{ env `JITSI_DOMAIN` }}/http-bind',
   hosts: {
     domain: '{{ env `JITSI_DOMAIN` }}',
     muc: 'conference.{{ env `JITSI_DOMAIN` }}',
     bridge: 'jitsi-videobridge.{{ env `JITSI_DOMAIN` }}',
   },
+
+  // Audio
+  enableNoAudioDetection: true,
+  enableNoisyMicDetection: true,
+
+  // Misc
+  channelLastN: 10,
+
+  // UI
   useNicks: false,
-  bosh: '//{{ env `JITSI_DOMAIN` }}/http-bind',
+  enableWelcomePage: true,
+
+  // Peer-To-Peer mode
+  p2p: {
+    enabled: true,
+    stunServers: [
+      { urls: 'stun:meet-jit-si-turnrelay.jitsi.net:443' }
+    ],
+    preferH264: true
+  },
 };
